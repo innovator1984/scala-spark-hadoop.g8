50907af29af38b06907bf13cb513848ea8fa9b55
Yuriy <basov@restream.rt.ru>
2017-10-04 15:26:15 +0300

    final commit

--- src/main/g8/build.sbt
+++ src/main/g8/build.sbt
@@ -1,9 +1,23 @@
-// give the user a nice default project!
-lazy val root = (project in file(".")).
-  settings(
-    inThisBuild(List(
-      organization := "com.example",
-      scalaVersion := "2.11.8"
-    )),
-    name := "My Template Project"
-  )
+import Dependencies._
+
+lazy val general = Seq(
+  name := "$name$",
+  organization := "$organization$",
+  version := "$version$",
+  scalaVersion := "$scala_version$"
+)
+
+lazy val dependencies = Seq(
+  sparkCore % "provided",
+  sparkSql % "provided",
+  hadoopClient % "provided",
+  configurator
+)
+
+lazy val root = (project in file("."))
+  .settings(
+    general,
+    libraryDependencies ++= dependencies,
+    dependencyOverrides ++= overrides,
+    assemblyJarName in assembly := name.value + "-" + version.value + ".jar"
+  )  
---
+++ src/main/g8/src/main/scala/ideas/Main.scala
@@ -0,0 +1,61 @@
+package ideas
+
+import org.apache.spark.sql.SparkSession
+
+/*
+ * COMMAND FOR LOCAL MODE:
+ * -XX:+UseG1GC -Xmx40g -Xms40g
+ *
+ *  # Set SPARK_MEM if it isn't already set since we also use it for this process
+ * SPARK_MEM=${SPARK_MEM:-512m}
+ * export SPARK_MEM
+
+ * # Set JAVA_OPTS to be able to load native libraries and to set heap size
+ * JAVA_OPTS="$OUR_JAVA_OPTS"
+ * JAVA_OPTS="$JAVA_OPTS -Djava.library.path=$SPARK_LIBRARY_PATH"
+ * JAVA_OPTS="$JAVA_OPTS -Xms$SPARK_MEM -Xmx$SPARK_MEM"
+ */
+
+/*
+ * COMMAND FOR CLUSTER MODE:
+#!/bin/bash 
+SPARK_HOME=$spark_home$ \ 
+HADOOP_CONF_DIR=$hadoop_home$ \ 
+$spark_home$/bin/spark-submit \ 
+--master yarn \ 
+--deploy-mode client \ 
+--driver-java-options "-Dhive.metastore.uris=thrift://$hive_server$" \ 
+--queue $scheduler_queue$ \
+$yarn_optimization$ \
+--class "ideas.Main" \ 
+./$jira_user$_$jira_task$_protey_$jira_extra$-$version$.jar
+*/
+
+object Main extends Serializable {
+
+  def initLog4j(): Unit = {
+    import org.apache.log4j.Logger
+    import org.apache.log4j.Level
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+  }
+
+  def main(args: Array[String]): Unit = {
+    println("Welcome to Spark!")
+    initLog4j()
+    lazy val spark: SparkSession = SparkSession.builder().appName("$jira_user$_$jira_task$_$jira_extra$")
+      //.config("spark.sql.warehouse.dir", "/tmp_spark/spark-warehouse")
+      //.config("spark.local.dir", "/tmp_spark/spark-temp")
+      .config("spark.driver.maxResultSize", "300g")
+      //.config("spark.worker.cleanup.enabled", "true")
+      //.config("spark.worker.cleanup.interval", "900")
+      .getOrCreate
+      // .master("local[*]").getOrCreate
+    spark.sparkContext.setLogLevel("ERROR")
+    import spark.implicits._
+
+    // %spark
+
+
+  }
+}
